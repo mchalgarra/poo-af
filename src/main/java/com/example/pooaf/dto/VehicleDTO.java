24fab75fab5ed64a47833e7d2a07c7930bdd5b09
Michell Algarra <michell.algarra@gmail.com>
2020-11-30 18:01:58 -0300

    Changed dailyValue constrain from 'NotBlank' to 'NotNull'

--- src/main/java/com/example/pooaf/dto/VehicleDTO.java
+++ src/main/java/com/example/pooaf/dto/VehicleDTO.java
@@ -2,6 +2,7 @@ package com.example.pooaf.dto;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 
@@ -10,7 +11,7 @@ public class VehicleDTO {
     @NotBlank(message = "Model required!")
     @Length(min = 2, max = 30, message = "It should be between 2 and 30 characters")
     private String model;
-    @NotBlank(message = "Daily value required!")
+    @NotNull(message = "Daily value required!")
     @Min(value = 0, message = "The value cannot be negative!")
     private double dailyValue;
 
